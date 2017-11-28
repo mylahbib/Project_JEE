df3b79e0ce33f481b085572c0d14a45502d25713
chama76 <chama7649@gmail.com>
2017-11-28 22:10:10 +0000

    new

---
+++ PaintApp/PaintApplication/src/painttools/Tool.java
@@ -0,0 +1,7 @@
+
+package painttools;
+
+public enum Tool {CIRCLE , ERASER  , BRUSH  ,
+                  SQUARE  , OVAL    , LINE   ,
+                  POLYGON , AIRBRUSH, PICKER , 
+                  BUCKET  , RECTANGLE, FILLER, ROUND_RECT };
